82977c200844c8995e15e78df3709a107b8377b2
Minkyu0424 <yng1404@gmail.com>
2024-06-13 15:11:10 +0900

    :memo:[memo]: Link, usePathname사용

---
+++ components/Navigation.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Navigation = () => {
+  const path = usePathname();
+  console.log(path);
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"/middle/basement"}>About Basement</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
