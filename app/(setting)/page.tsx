855b66500550f021d9e824208c418b3efb62102b
Minkyu0424 <yng1404@gmail.com>
2024-06-26 19:07:19 +0900

    :sparkles: [feat]: 메타데이터 활용

--- app/(setting)/page.tsx
+++ app/(setting)/page.tsx
@@ -1,5 +1,14 @@
 const page = () => {
-  return <div>hi</div>;
+  return (
+    <div>
+      <ul>
+        <li>메타 데이터는 서버 컴포넌트에서만 존재 가능!</li>
+        <li>메타 데이터는 서버 컴포넌트에서만 존재 가능!</li>
+        <li>메타 데이터는 서버 컴포넌트에서만 존재 가능!</li>
+        <li>메타 데이터는 서버 컴포넌트에서만 존재 가능!</li>
+      </ul>
+    </div>
+  );
 };
 
 export default page;
